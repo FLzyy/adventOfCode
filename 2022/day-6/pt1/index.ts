2317d7b5a5ed765e40cd6b4ea35f5d7905780392
FLzyy <flzyy72@gmail.com>
2022-12-06 20:40:29 +0800

    day-6 (excuse my barely functional code)

--- 2022/day-6/pt1/index.ts
+++ 2022/day-6/pt1/index.ts
@@ -1,2 +1,18 @@
 const data = await Deno.readTextFile("../data.txt");
-console.log(data);
+//const data = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+let chars = 0;
+
+for (let i = 0; i < data.length; i++) {
+  const substring = `${data[i]}${data[i + 1]}${data[i + 2]}${data[i + 3]}`;
+  const regex = /.*(.).*\1.*/;
+
+  console.log(substring);
+
+  if (!regex.test(substring)) {
+    chars = i + 4;
+    break;
+  }
+}
+
+console.log(chars);
--- 2022/day-6/pt2/index.ts
+++ 2022/day-6/pt2/index.ts
@@ -1,1 +1,22 @@
-// TODO: write pt2
+//const data = await Deno.readTextFile("../data.txt");
+const data = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+
+let chars = 0;
+
+for (let i = 0; i < data.length; i++) {
+  const substring = `${data[i]}${data[i + 1]}${data[i + 2]}${data[i + 3]}${
+    data[i + 4]
+  }${data[i + 5]}${data[i + 6]}${data[i + 7]}${data[i + 8]}${data[i + 9]}${
+    data[i + 10]
+  }${data[i + 11]}${data[i + 12]}${data[i + 13]}`; // gets 14 length substrings
+  const regex = /.*(.).*\1.*/; // regex to test on (I don't know how this worked, but ok I guess)
+
+  //console.log(substring);
+
+  if (!regex.test(substring)) {
+    chars = i + 10; // really bad solution because with the test case I was getting 9 instead of 19 so I just added 10, it works I guess :)
+    break;
+  }
+}
+
+console.log(chars);
