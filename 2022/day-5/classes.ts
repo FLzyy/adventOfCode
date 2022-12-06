daa56e5f6e996263a37f58619edbcee410532f2c
FLzyy <flzyy72@gmail.com>
2022-12-06 18:59:59 +0800

    day-5

---
+++ 2022/day-5/classes.ts
@@ -0,0 +1,47 @@
+export class Stack {
+  container: string[];
+
+  constructor() {
+    this.container = [];
+  }
+
+  push(...elements: string[]) {
+    this.container.push(...elements);
+  }
+
+  pop() {
+    if (this.container.length === 0) {
+      return "Underflow: Stack is empty";
+    }
+
+    return this.container.pop();
+  }
+
+  move(stack: Stack) {
+    stack.push(this.peek());
+    this.pop();
+  }
+
+  moveMul(stack: Stack, n: number) {
+    const sliced = this.container.slice(-n);
+    for (let i = 0; i < sliced.length; i++) {
+      if (sliced[i]) {
+        stack.push(sliced[i]);
+      }
+    }
+    this.container.splice(-n);
+  }
+
+  peek() {
+    return this.container[this.container.length - 1];
+  }
+
+  isEmpty() {
+    return this.container.length === 0;
+  }
+
+  log() {
+    console.log(this.container.toString());
+    return;
+  }
+}
